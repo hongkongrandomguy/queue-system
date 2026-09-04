ce252bc42d0b1d4757ce8fe481f29925a09f9c27
hongkongrandomguy <hong.kong.random.guy@gmail.com>
2026-09-04 21:14:29 +0800

    Create config.js

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMBiSM4oGSDE3khRISIxtxDCJJTLaxm0",
+  authDomain: "ktmc-queue-system.firebaseapp.com",
+  databaseURL: "https://ktmc-queue-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ktmc-queue-system",
+  storageBucket: "ktmc-queue-system.firebasestorage.app",
+  messagingSenderId: "1098822703041",
+  appId: "1:1098822703041:web:c010a95bf5c71c478a0140"
+};
